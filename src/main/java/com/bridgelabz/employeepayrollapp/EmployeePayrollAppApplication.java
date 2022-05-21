9d0585a956ef090f447fe7793c32ef94c5734a2e
unknown <SmithaRai.com>
2022-05-21 08:14:06 +0530

    Added : created employeepayroll app project

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.employeepayrollapp;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+        run(EmployeePayrollAppApplication.class, args);
+    }
+
+}
